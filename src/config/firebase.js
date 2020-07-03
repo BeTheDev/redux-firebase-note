390e48e8e529be91031ac7552ccb3542e6831ecc
daonez <cyongchoi@gmail.com>
2020-07-03 23:07:46 +0900

    add firebase,redux

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+  measurementId: process.env.REACT_MESUREMENT_ID
+}
+
+firebase.inititalizeApp(firebaseConfig)
+const database=firebase.firestore()
+
+export default database
