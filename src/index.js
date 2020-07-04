f0766363f548bfee32ab2f84b41987e5744d40ca
daonez <cyongchoi@gmail.com>
2020-07-04 23:26:58 +0900

    configure firebase to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore } from "redux"
+import firebase from "./firebase"
+import { ReactReduxFirebaseProvider } from "react-redux-firebase"
+import { createFirestoreInstance } from "redux-firestore"
+import { rootReducer } from "./redux/reducers"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
 
+const initialState = {}
+const store = createStore(rootReducer, initialState)
 
+const firebaseDatabaseProps = {
+  firebase,
+  firebaseDatabaseConfig: {
+    userProfile: "users",
+    useFirestoreForProfile: true,
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...firebaseDatabaseProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
