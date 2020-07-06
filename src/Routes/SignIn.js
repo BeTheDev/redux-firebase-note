2c093ae4161b69886655058459f8d1a545f52128
daonez <cyongchoi@gmail.com>
2020-07-06 23:17:03 +0900

    add login

---
+++ src/Routes/SignIn.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { useFirebase } from "react-redux-firebase"
+import { useHistory } from "react-router-dom"
+
+const SignIn = () => {
+  const firebase = useFirebase()
+  const history = useHistory()
+}
+
+const googleSignIn = () => {
+  firebase
+    .login({
+      provider: "google",
+      type: "popup",
+    })
+    .then(() => {
+      hisotory.push("./todos")
+    })
+}
+return (
+  <div>
+    <h1>Sign In</h1>
+    <button
+      onClick={(e) => {
+        e.preventDefault()
+        googleSignIn()
+      }}
+    >
+      Sign In with Google
+    </button>
+  </div>
+)
+
+export default SignIn
