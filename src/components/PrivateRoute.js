ae980344f12c9f36261615e41a155c7cff493c52
daonez <cyongchoi@gmail.com>
2020-07-11 23:52:09 +0900

    privateroute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,39 +1,26 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { isLoaded, isEmpty } from "react-redux-firebase"
 import { useSelector } from "react-redux"
 
 const PrivateRoute = ({ children, ...remainingProps }) => {
   const auth = useSelector((state) => state.firebase.auth)
-
-  console.log(auth)
-  useEffect(() => {
-    const isLoggedIn = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user)
-      } else {
-        setUser(false)
+  return (
+    <Route
+      {...remainingProps}
+      render={({ location }) =>
+        isLoaded(auth) && !isEmpty(auth) ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location },
+            }}
+          />
+        )
       }
-    }, [])
-
-    return (
-      <Route
-        {...remainingProps}
-        render={({ location }) =>
-          isLoaded(auth) && !isEmpty(auth) ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location },
-              }}
-            />
-          )
-        }
-      />
-    )
-  })
+    />
+  )
 }
-
 export default PrivateRoute
